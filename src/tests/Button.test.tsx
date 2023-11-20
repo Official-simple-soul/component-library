af89df5408b1d45bca7fe3d69ad4892cec6f7312
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2023-11-20 15:56:44 +0100

    ch: component library testing

---
+++ src/tests/Button.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from "../components";
+
+export {};
+
+test("renders learn react link", () => {
+  render(<Button label='' />);
+});
